a8e41ce8dc694eed117082231f741a94b57fa01f
Mansoor Ali <mali@panteragroup.org>
2021-10-16 18:06:19 +0500

    added SPM

---
+++ Sources/MyLibraryCore/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  MyLibrary
+//
+//  Created by Mansoor Ali on 16/10/2021.
+//
+
+import Foundation
+
+public struct User {
+
+}
---
+++ Sources/MyLibraryStore/UserStore.swift
@@ -0,0 +1,13 @@
+//
+//  UserStore.swift
+//  MyLibrary
+//
+//  Created by Mansoor Ali on 16/10/2021.
+//
+
+import Foundation
+
+public protocol UserStore {
+	var user: String {get}
+}
+
